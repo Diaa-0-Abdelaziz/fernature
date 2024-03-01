2859464256e296ef86a2740a2aa4094e7cbbbbae
Diaa-0-Abdelaziz <diaamohamedabelaziz@gmail.com>
2024-03-01 11:29:45 +0200

    modified:   src/components/Subscribe/Subscripe.jsx

--- src/components/Subscribe/Subscripe.jsx
+++ src/components/Subscribe/Subscripe.jsx
@@ -8,7 +8,7 @@ export default function Subscripe() {
      <h4>Join Now For Early Access</h4>
      <p>Suspendisse eu velit est. Cras nec vestibulum quam. Donec tincidunt purus nec enim tincidunt, sit</p>
      <div className='form row w-100 p-3 justify-content-center'>
-           <div className="col-md-5">
+           <div className="col-md-5 mb-3">
             <input type="email" placeholder='Enter your email address...'/>
            </div>
         <div className='col-md-2'><span>subscripe</span></div>
